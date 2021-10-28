d4dad19aca6b9da81cce2e61cef5e265aa59ea75
David Luna González <lumiazaine@gmail.com>
2021-10-28 16:30:14 +0200

    Add files via upload

--- git.ps1
+++ git.ps1
@@ -1,6 +1,27 @@
-
+# Variables
 $enlace = Read-Host -Prompt "Inserta el enlace"
+$name = Read-Host -Prompt "Inserta el nombre de usuario"
+$email = Read-Host -Prompt "Inserta el correo electrónico"
+$nrama = Read-Host -Prompt "Inserta el nombre para la rama"
 
+
+# Configuración usuario y correo git
+
+$username = "git config --global user.name $name" 
+Invoke-Expression -Command:$username
+
+$usermail = "git config --global user.email $email"
+Invoke-Expression -Command:$usermail
+
+
+# Crear rama git
+
+$ramanueva = "git checkout -b $nrama"
+git branch $nrama
+Invoke-Expression -Command:$ramanueva
+
+
+# Inicializar rama git.
 
 git init
 
@@ -12,4 +33,4 @@ $Comando = "git remote add origin $enlace"
 
 Invoke-Expression -Command:$Comando
 
-git push -u origin master
+git push -u origin main
